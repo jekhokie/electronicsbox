c72119bd6df688306746a3e13f9a93b5ca75a165
Justin Karimi <jekhokie@gmail.com>
2018-12-26 12:32:36 -0500

    Fix LED Pin Blink Project
    Fix reference to a global variable in the blinking LED project.

--- 0-blink-led/main.py
+++ 0-blink-led/main.py
@@ -20,8 +20,11 @@ GPIO.setwarnings(False)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 # continuously blink LED
-while True:
-    GPIO.output(led_pin, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(led_pin, GPIO.LOW)
-    time.sleep(1)
+try:
+    while True:
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(LED_PIN, GPIO.LOW)
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
