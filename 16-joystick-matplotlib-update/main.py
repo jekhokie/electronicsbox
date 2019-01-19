c09eb2bd93ae57c736f576798eaf2634478a5ef9
Justin Karimi <jekhokie@gmail.com>
2019-01-18 20:03:15 -0500

    Integrate Joystick and Fix Normalization
    Integrate the Analog joystick, ADC inputs, and fix the normalization
    calculation to correctly center on zero and output the correct
    normalized values for the joystick range.

--- 16-joystick-matplotlib-update/main.py
+++ 16-joystick-matplotlib-update/main.py
@@ -5,7 +5,9 @@
 # range of 0-255 in either forward or reverse direction.
 
 from __future__ import division
-import datetime as dt
+import busio
+import digitalio
+import board
 ### WORKAROUND: FOR MAC OSX ISSUES ###
 import matplotlib
 matplotlib.use('TkAgg')
@@ -13,17 +15,18 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.patches as patches
-import random
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
 
 # configurations for tuning the script
 MIN_X = 0               # analog sensor x-axis minimum value
 MAX_X = 65472           # analog sensor x-axis maximum value
 MIN_Y = 0               # analog sensor y-axis minimum value
 MAX_Y = 65472           # analog sensor y-axis maximum value
-STABLE_X_SIZE = 5000    # 'quiet' area X size/range
-STABLE_Y_SIZE = 5000    # 'quiet' area Y size/range
-SENSOR_READ_MS = 500    # sleep time (in ms) between sensor reads
-MOTOR_MIN = 0           # minimum value possible for motor control
+STABLE_X_SIZE = 5000    # 'dead' area X size/range
+STABLE_Y_SIZE = 5000    # 'dead' area Y size/range
+SENSOR_READ_MS = 100    # sleep time (in ms) between sensor reads
+MOTOR_MIN = -255        # minimum value possible for motor control
 MOTOR_MAX = 255         # maximum value possible for motor control
 
 # create plot figure with boundaries
@@ -32,22 +35,37 @@ ax = fig.add_subplot(1, 1, 1)
 ax.set_xlim(0, MAX_X)
 ax.set_ylim(0, MAX_Y)
 
-# scale the analog sensor read to a value a motor expects
+# create SPI bus, chip select, and resulting mcp object
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs = digitalio.DigitalInOut(board.D5)
+mcp = MCP.MCP3008(spi, cs)
+
+# create analog input on channel 0 for x-axis, channel 1 for y-axis
+x_chan = AnalogIn(mcp, MCP.P0)
+y_chan = AnalogIn(mcp, MCP.P1)
+
 def scale_to_motor(value, min_value, max_value):
-    new_value = (MOTOR_MAX - MOTOR_MIN) / (max_value - min_value) * (value - min_value) + MOTOR_MIN
+    '''
+    Scale the incoming value to the range of the min and max for the
+    motors, using the following formula to normalize in [a, b] (for
+    instance, [-255, 255] for reverse to forward motor control):
+                             (x - min_x)
+        x_norm = (b - a) * --------------- + a
+                           (max_x - min_x)
+    '''
+
+    new_value = (MOTOR_MAX - MOTOR_MIN) * ( (value - min_value) / (max_value - min_value) ) + MOTOR_MIN
     return int(new_value)
 
-# main update loop
 def update_graph(i, xs, ys, x_start, x_size, y_start, y_size):
     '''
     Dynamically update plot with sensor value and dashed rectangle
-    indicating the "quiet zone" (x_start, x_size, y_start, y_size)
+    indicating the "dead zone" (x_start, x_size, y_start, y_size)
     '''
 
-    ## DEBUG CODE: REPLACE WITH SENSOR READ VALUES ##
-    x_value = random.randint(-MAX_X, MAX_X)
-    y_value = random.randint(-MAX_Y, MAX_Y)
-    #################################################
+    # read analog joystick values
+    x_value = x_chan.value
+    y_value = y_chan.value
 
     # convert to motor-expected scale
     scaled_x = scale_to_motor(x_value, MIN_X, MAX_X)
@@ -67,22 +85,18 @@ def update_graph(i, xs, ys, x_start, x_size, y_start, y_size):
     ax.plot(scaled_x, scaled_y, "bs")
     ax.annotate(str("{}, {}".format(scaled_x, scaled_y)), (scaled_x + 5.0, scaled_y + 5.0))
 
-    # plot the "quiet" zone where no movement is expected
-    quiet_zone = patches.Rectangle((x_start, y_start), x_size, y_size,
+    # plot the "dead" zone where no movement is expected
+    dead_zone = patches.Rectangle((x_start, y_start), x_size, y_size,
                                    linestyle='--', linewidth=1, edgecolor='r', facecolor='none')
-    ax.add_patch(quiet_zone)
+    ax.add_patch(dead_zone)
 
-# calculate the center/resting position of the joystick analog value
-mid_x = (MAX_X - MIN_X) / 2.0
-mid_y = (MAX_Y - MIN_Y) / 2.0
-
-# calculate the scaled components for the quiet zone for the joystick
-stable_x_size = scale_to_motor(STABLE_X_SIZE, MIN_X, MAX_X)
-stable_y_size = scale_to_motor(STABLE_Y_SIZE, MIN_Y, MAX_Y)
+# calculate the scaled components for the dead zone for the joystick
+stable_x_size = scale_to_motor(STABLE_X_SIZE, MIN_X, MAX_X) / 2
+stable_y_size = scale_to_motor(STABLE_Y_SIZE, MIN_Y, MAX_Y) / 2
 stable_x_start = -(stable_x_size / 2.0)
 stable_y_start = -(stable_y_size / 2.0)
 
 # dynamic update on interval
-ani = animation.FuncAnimation(fig, update_graph, fargs=(0, 0, stable_x_start, stable_x_size, stable_y_start, stable_y_size),
-                              interval=SENSOR_READ_MS)
+ani = animation.FuncAnimation(fig, update_graph, fargs=(0, 0, stable_x_start, stable_x_size,
+                              stable_y_start, stable_y_size), interval=SENSOR_READ_MS)
 plt.show()
